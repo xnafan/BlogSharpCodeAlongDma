99e3527f9821026d51158cdc71bc78234c60e986
Jakob Farian Krarup <kr@rup.dk>
2022-11-16 10:26:50 +0100

    Details added

--- BlogSharpCodeAlongDmaWeb/Controllers/BlogPostController.cs
+++ BlogSharpCodeAlongDmaWeb/Controllers/BlogPostController.cs
@@ -2,13 +2,12 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BlogSharpCodeAlongDmaWeb.Controllers
+namespace BlogSharpCodeAlongDmaWeb.Controllers;
+public class BlogPostController : Controller
 {
-    public class BlogPostController : Controller
-    {
-        List<BlogPost> _blogPosts = new List<BlogPost>() {
-            new BlogPost() {Id=1, Title="Awesome blogpost", 
-                Content="Also some awesome content", 
+    List<BlogPost> _blogPosts = new List<BlogPost>() {
+            new BlogPost() {Id=1, Title="Awesome blogpost",
+                Content="Also some awesome content",
                 CreationDate = DateTime.Now.AddDays(-3) },
             new BlogPost() {Id=2, Title="Another blogpost",
                 Content="More awesome content",
@@ -19,80 +18,74 @@ namespace BlogSharpCodeAlongDmaWeb.Controllers
 
 
         };
+    public ActionResult Index() => View(_blogPosts);
 
-        // GET: BlogPostController
-        public ActionResult Index()
+    // GET: BlogPostController/Details/5
+    public ActionResult Details(int id)
+    {
+        return View();
+    }
+
+    // GET: BlogPostController/Create
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: BlogPostController/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
         {
             return View();
         }
+    }
 
-        // GET: BlogPostController/Details/5
-        public ActionResult Details(int id)
+    // GET: BlogPostController/Edit/5
+    public ActionResult Edit(int id)
+    {
+        return View();
+    }
+
+    // POST: BlogPostController/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(int id, IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
         {
             return View();
         }
+    }
 
-        // GET: BlogPostController/Create
-        public ActionResult Create()
+    // GET: BlogPostController/Delete/5
+    public ActionResult Delete(int id)
+    {
+        return View();
+    }
+
+    // POST: BlogPostController/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Delete(int id, IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
         {
             return View();
-        }
-
-        // POST: BlogPostController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: BlogPostController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: BlogPostController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: BlogPostController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: BlogPostController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
